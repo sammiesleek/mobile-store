d5e75c60e1922ab6aa034d0d49b71af54a358c3e
sammiesleek <samlocomotive1@gmail.com>
2022-03-03 21:51:57 -0800

    finished home page

--- dist/js/script.js
+++ dist/js/script.js
@@ -30,4 +30,54 @@ $(document).ready(function(){
             }
         }
     })
+    // isotope filter 
+    var $grid= $(".grid").isotope({
+        itemSelector:'.grid-item',
+        layoutMode:'fitRows'
+    })
+    $(".button-group").on("click", "button",function(){
+        var filterValue=$(this).attr('data-filter');
+        $grid.isotope({filter:filterValue})
+    })
+
+
+    // new phones section 
+      $("#new-phones .owl-carousel").owlCarousel({
+        loop:true,
+        nav: false,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            },
+           
+            1500:{
+                items:7
+            }
+        }
+    })
+
+    // blogs carousel 
+      $("#blogs .owl-carousel").owlCarousel({
+        loop:true,
+        nav: false,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            900:{
+                items:4
+            },
+        }
+    })
 })
